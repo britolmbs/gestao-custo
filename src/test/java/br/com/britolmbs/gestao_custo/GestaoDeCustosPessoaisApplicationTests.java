7216d7046681f577c9365003876cd4bf9f452e15
Lucas Brito <britolmbs@hotmail.com>
2025-10-27 16:35:12 -0300

    Feat: iniciando o Projeto

---
+++ src/test/java/br/com/britolmbs/gestao_custo/GestaoDeCustosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.britolmbs.gestao_custo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCustosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
