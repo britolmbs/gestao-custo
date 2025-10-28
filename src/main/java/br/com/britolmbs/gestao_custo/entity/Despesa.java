577f884dbd94d65f02e5ea2099cc28f3c10d52ea
Lucas Brito <britolmbs@hotmail.com>
2025-10-28 17:18:02 -0300

    Feat: Criação do Controller e Entity

---
+++ src/main/java/br/com/britolmbs/gestao_custo/entity/Despesa.java
@@ -0,0 +1,82 @@
+package br.com.britolmbs.gestao_custo.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name= "despesa")
+public class Despesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String descricao;
+    private LocalDate data;
+    private BigDecimal valor;
+    private String categoria;
+    private String email;
+
+    @CreatedDate
+    private LocalDate data_criacao;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(LocalDate data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+}
