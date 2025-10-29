ef5a1189c140bdc622691d7723ff3ff4a03f1c23
Lucas Brito <britolmbs@hotmail.com>
2025-10-29 16:19:28 -0300

    Feat: Criação do UseCases

--- src/main/java/br/com/britolmbs/gestao_custo/controller/GestaoDespesaController.java
+++ src/main/java/br/com/britolmbs/gestao_custo/controller/GestaoDespesaController.java
@@ -1,13 +1,18 @@
 package br.com.britolmbs.gestao_custo.controller;
 
+import br.com.britolmbs.gestao_custo.entity.Despesa;
+import br.com.britolmbs.gestao_custo.useCases.CadastroDespesaUseCase;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/gestao")
+@RestController
 public class GestaoDespesaController {
 
     @PostMapping("/create")
-    public void create() {
-
+    public void create(Despesa despesa) {
+        CadastroDespesaUseCase cadastroDespesaUseCase = new CadastroDespesaUseCase();
+        cadastroDespesaUseCase.execute(despesa);
     }
 }
---
+++ src/main/java/br/com/britolmbs/gestao_custo/useCases/CadastroDespesaUseCase.java
@@ -0,0 +1,10 @@
+package br.com.britolmbs.gestao_custo.useCases;
+
+import br.com.britolmbs.gestao_custo.entity.Despesa;
+
+public class CadastroDespesaUseCase {
+
+    public void execute(Despesa despesa){
+        System.out.println(despesa);
+    }
+}
