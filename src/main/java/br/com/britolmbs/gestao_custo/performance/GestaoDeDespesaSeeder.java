377738840e517291207e892d41ea4e846e8b1a2e
Lucas Brito <britolmbs@hotmail.com>
2025-11-11 16:14:41 -0300

    Feat: criando o Seed

---
+++ src/main/java/br/com/britolmbs/gestao_custo/performance/GestaoDeDespesaSeeder.java
@@ -0,0 +1,36 @@
+package br.com.britolmbs.gestao_custo.performance;
+
+
+import br.com.britolmbs.gestao_custo.entity.Despesa;
+import br.com.britolmbs.gestao_custo.repository.DespesaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GestaoDeDespesaSeeder implements CommandLineRunner {
+
+    @Autowired
+    DespesaRepository despesaRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Despesa> despesas = new ArrayList<>();
+        for (int i =0; i < 1000; i++){
+            Despesa despesa = new Despesa();
+            despesa.setDescricao("Gasto nº: " + i);
+            despesa.setValor(BigDecimal.valueOf(10 + (i % 50)));
+            despesa.setData(LocalDate.now().minusDays((i % 30)));
+            despesa.setCategoria("Teste");
+            despesa.setEmail("performance@teste.com");
+            despesas.add(despesa);
+
+        }
+        despesaRepository.saveAll(despesas);
+    }
+}
