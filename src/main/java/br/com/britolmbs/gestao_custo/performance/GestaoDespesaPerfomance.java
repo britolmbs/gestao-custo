a0df31b3d369f3695f93090ec5991c79b4f2a6a4
Lucas Brito <britolmbs@hotmail.com>
2025-11-19 14:56:25 -0300

    Feat: Retornando com paginação e por email

--- src/main/java/br/com/britolmbs/gestao_custo/performance/GestaoDespesaPerfomance.java
+++ src/main/java/br/com/britolmbs/gestao_custo/performance/GestaoDespesaPerfomance.java
@@ -3,8 +3,12 @@ package br.com.britolmbs.gestao_custo.performance;
 import br.com.britolmbs.gestao_custo.entity.Despesa;
 import br.com.britolmbs.gestao_custo.repository.DespesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,30 @@ public class GestaoDespesaPerfomance {
         System.out.println("Tempo (sem paginação): "+ (fim - inicio) + " ms");
 
        return ResponseEntity.ok(despesas);
+    }
+
+    @GetMapping("/com-paginacao")
+    public ResponseEntity<Page<Despesa>> listarComPaginacao(Pageable pageable){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        var despesas = repository.findAll(pageable);
+        stopWatch.stop();
+
+
+        System.out.println("Tempo (com Paginação): " + stopWatch.getTotalTimeMillis() + " ms");
+        return ResponseEntity.ok(despesas);
+    }
+
+    @GetMapping("/com-paginacao/{email}")
+    public ResponseEntity<Page<Despesa>> listarPaginacaoEmail(@PathVariable String email, Pageable pageable){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        var despesas = repository.findByEmail(email, pageable);
+        stopWatch.stop();
+
+        System.out.println("Tempo (Com Paginação e Email): " + stopWatch.getTotalTimeMillis() + " ms");
+        return ResponseEntity.ok(despesas);
     }
 
 }
--- src/main/java/br/com/britolmbs/gestao_custo/repository/DespesaRepository.java
+++ src/main/java/br/com/britolmbs/gestao_custo/repository/DespesaRepository.java
@@ -1,6 +1,8 @@
 package br.com.britolmbs.gestao_custo.repository;
 
 import br.com.britolmbs.gestao_custo.entity.Despesa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -11,4 +13,5 @@ public interface DespesaRepository extends JpaRepository<Despesa, UUID> {
 
     List<Despesa> findByEmail(String email);
     List<Despesa> findByEmailAndData(String email, LocalDate data);
+    Page<Despesa> findByEmail(String email, Pageable pageable);
 }
